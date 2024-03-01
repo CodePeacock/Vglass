933b4e0cedc94c520c86177993e163a42548d832
Mayur Sinalkar <mayursinalkar404@gmail.com>
2024-03-01 20:52:05 +0530

    Add video streaming functionality

--- main.py
+++ main.py
@@ -2,13 +2,51 @@ import contextlib
 from functools import partial
 from os import listdir
 from os.path import exists
+from typing import Any, Generator
 
 import cv2
 import dlib
 import numpy as np
+from flask import Flask, Response, render_template
 
 from compare_source import draw as comp_draw
 from project_tools import FaceModel3D, dist_bt_2_face, getDlibLandmark, headPose
+
+app = Flask(__name__)
+
+
+def generate_frames() -> Generator[bytes, Any, None]:
+    cap = cv2.VideoCapture(
+        0
+    )  # Make sure to provide the correct path to your video file
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            break
+
+        _, buffer = cv2.imencode(".jpg", frame)
+        frame = buffer.tobytes()
+
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+
+        cv2.waitKey(
+            int(1000 / cap.get(cv2.CAP_PROP_FPS))
+        )  # Introduce delay based on fps
+
+    cap.release()
+
+
+@app.route("/")
+def index() -> str:
+    return render_template("index.html")
+
+
+@app.route("/video_feed")
+def video_feed() -> Response:
+    return Response(
+        generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
 
 
 class glassesProject:
--- project_tools/__init__.py
+++ project_tools/__init__.py
@@ -1,3 +1,3 @@
-from .tools import *
-from .headPose import *
 from .face_model_3D_object import *
+from .headPose import *
+from .tools import *
